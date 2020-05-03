5abbf5f3fd46a18845fb681ef787dc65bfdc2194
mohamadreza dakhili <mohamadrezadakhili53@gmail.com>
2020-05-03 17:16:09 +0430

    add AOS library

---
+++ src/index.js
@@ -0,0 +1,6 @@
+AOS.init({
+  anchorPlacement: "top-bottom",
+  delay: 100,
+  offset: 100, // offset (in px) from the original trigger poin
+  duration: 800, // values from 0 to 3000, with step 50ms
+});
